baa707f2a0af089d8789420f5a7b9de68afac831
yuvi0049 <yuvi0049@gmail.com>
2018-02-23 21:06:47 +0530

    part 3 completed

---
+++ continentGDP.js
@@ -0,0 +1,135 @@
+
+// set the dimensions of the canvas
+var margin = {top: 20, right: 20, bottom: 70, left: 80},
+    width = 600 - margin.left - margin.right,
+    height = 350 - margin.top - margin.bottom;
+
+// set the ranges
+var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+
+var y = d3.scale.linear().range([height, 0]);
+
+
+// define the axis
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom")
+
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .ticks(10);
+// add the SVG element
+var svg4 = d3.select(".sec4").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", 
+          "translate(" + margin.left + "," + margin.top + ")");
+
+
+// load the data
+d3.json("continentGDP.json", function(error, data) {
+
+    data.forEach(function(d) {
+     // continentObject[0].arr[1]
+        d.arr[0] = d.arr[0];
+        d.arr[2] = +d.arr[2];
+    });
+    
+  // scale the range of the data
+  x.domain(data.map(function(d) { return d.arr[0]; }));
+  y.domain([0, d3.max(data, function(d) { return d.arr[2]; })]);
+
+  // add axis
+  svg4.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+    .selectAll("text")
+      .style("text-anchor", "end")
+      .attr("dx", "-.8em")
+      .attr("dy", "-.55em")
+      .attr("transform", "rotate(-80)");
+
+ svg4.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("x", -height/2)
+        .attr("dy", "-6em")
+        .style("text-anchor", "middle")
+        .text("GDP(Country)");
+
+
+  // Add bar chart
+  svg4.selectAll("bar")
+      .data(data)
+    .enter().append("rect")
+      .attr("class", "bar")
+      //.transition().duration(3000)
+      .attr("x", function(d) { return x(d.arr[0]); })
+      .attr("width", x.rangeBand())
+      .attr("y", function(d) { return y(d.arr[2]); })
+      .attr("height", function(d) { return height - y(d.arr[2]); })
+      .style("fill", function(d,i) { return 'rgb(80, 20, ' + ((i * 30) + 100) + ')'});
+
+});
+
+
+
+// svg1.selectAll('rect')
+//     .data(data)
+//     .enter()
+//     .append('rect')
+//     .attr("height", 0)
+//     .attr("y", height)
+//     .transition().duration(3000)
+//     .delay( function(d,i) { return i * 200; })
+//     // attributes can be also combined under one .attr
+//     .attr({
+//       "x": function(d) { return xScale(d.Country); },
+//       "y": function(d) { return yScale(d.Population2013); },
+//       "width": xScale.rangeBand(),
+//       "height": function(d) { return  height - yScale(d.Population2013); }
+//     })
+//     .style("fill", function(d,i) { return 'rgb(20, 20, ' + ((i * 30) + 100) + ')'});
+
+
+//         svg2.selectAll('text')
+//             .data(data)
+//             .enter()
+//             .append('text')
+
+
+
+//             .text(function(d){
+//                 return d.Population2013;
+//             })
+//             .attr({
+//                 "x": function(d){ return xScale(d.Country) +  xScale.rangeBand()/2; },
+//                 "y": function(d){ return yScale(d.Population2013)+ 12; },
+//                 "font-family": 'sans-serif',
+//                 "font-size": '13px',
+//                 "font-weight": 'bold',
+//                 "fill": 'white',
+//                 "text-anchor": 'middle'
+//            });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- csvJson.js
+++ csvJson.js
@@ -8,6 +8,10 @@ const reader = readline.createInterface(instream, outstream);
 const populationObject = [];
 const gdpObject = [];
 const ppObject = [];
+
+const continentObject = [];
+
+
 let headers = [];
 let headerCounter = 0;
 reader.on('line', (line) => {
@@ -15,42 +19,24 @@ reader.on('line', (line) => {
     headers = line.split(',');
     headerCounter = 1;
   } else {
-
-
-    jsonConversion(line);
-
-
-    
+    jsonConversion(line); 
   }
-
   sortElement();
-
-  
 });
-
-
-
-
-
 
 reader.on('close', () => {
-
-  writeToFile();
-
-
+  getFinalContinent();
+  writeToFile();  
   
 });
-
-
-
-
-
-
 
 function jsonConversion(line){
 
   const currentline = line.split(',');
     // console.log(currentline);
+    var continent = 6;
+    var gdpValue;
+    var populationValue;
     if (!(currentline[0] === 'European Union' || currentline[0] === 'World')) {
       const tempValueForPopulation = {};
       const tempValueForGdp = {};
@@ -60,18 +46,29 @@ function jsonConversion(line){
           tempValueForPp[headers[val]] = currentline[val];
           tempValueForPopulation[headers[val]] = currentline[val];
           tempValueForGdp[headers[val]] = currentline[val];
+          continent = checkContinent(currentline[val]);
+         // console.log(continent);
         }
-        if (headers[val] === 'GDP2013') { tempValueForGdp[headers[val]] = currentline[val]; }
-        if (headers[val] === 'Population2013') { tempValueForPopulation[headers[val]] = currentline[val]; }
-        if (headers[val] === 'PPP2013') { tempValueForPp[headers[val]] = currentline[val]; }
+        if (headers[val] === 'GDP2013') { 
+          tempValueForGdp[headers[val]] = currentline[val];
+          gdpValue = val;
+           }
+        if (headers[val] === 'Population2013') { 
+          tempValueForPopulation[headers[val]] = currentline[val]; 
+          populationValue = val;
+        }
+        if (headers[val] === 'PPP2013') { 
+          tempValueForPp[headers[val]] = currentline[val];
+
+         }
       }
       populationObject.push(tempValueForPopulation);
       gdpObject.push(tempValueForGdp);
       ppObject.push(tempValueForPp);
+
+      computeGDP(continent,currentline,gdpValue,populationValue);
     }
 }
-
-
 
 function writeToFile(){
 
@@ -84,9 +81,11 @@ function writeToFile(){
   fs.writeFile('pp.json', JSON.stringify(ppObject), 'utf8', (err) => {
     console.log(err);
   });
+  fs.writeFile('continentGDP.json', JSON.stringify(continentObject), 'utf8', (err) => {
+    console.log(err);
+  });
+ // console.log(continentObject);
 }
-
-
 
 function sortElement(){
 
@@ -96,31 +95,119 @@ function sortElement(){
 }
 
 
-function csvTojson(csv){
 
 
 
 
+
+
+
+function checkContinent(Country){
+
+
+
+
+  if (Country === 'Russia' || Country === 'India' || Country === 'Japan' || Country === 'China' || Country === 'Indonesia' || Country === 'Saudi Arabia' || Country === 'Turkey' || Country === 'Republic of Korea') {
+           // continentObject[0]['continent'] = "Asia";
+            return 0;
+        } else if (Country === 'France' || Country === 'Italy' || Country === 'Germany' || Country === 'United Kingdom') {
+           // continentObject[0]['continent'] = "Asia";
+            return 1;
+        } else if (Country === 'Australia') {
+          // continentObject[0]['continent'] = "Asia";
+           return 2;
+        } else if (Country === 'Mexico' || Country === 'USA' || Country === 'Canada') {
+          //  continentObject[0]['continent'] = "Asia";
+            return 3;
+        } else if (Country === 'Brazil' || Country === 'Argentina') {
+          //  continentObject[0]['continent'] = "Asia";
+            return 4;
+        } else if (Country === 'South Africa') {
+          // continentObject[0]['continent'] = "Asia";
+            return 5;
+        }
+}
+
+var temp = {};
+
+var asia = ['Asia',0,0];
+var nAmerica = ['North America',0,0];
+var sAmerica = ['South America',0,0];
+var Australia = ['Australia',0,0];
+var Europe = ['Europe',0,0];
+var Africa = ['Africa',0,0];
+
+
+
+
+function computeGDP(continent,currentline,gdpValue,populationValue)
+{
+
+ // console.log(continentObject);
+
+  // continentObject[continent]['gdp'] += +currentline[gdpValue];
+  // continentObject[continent]['population'] += +currentline[populationValue];
+
+
+
+  if(continent == 0){
+    asia[1] += parseFloat(currentline[populationValue]);
+    asia[2] += parseFloat(currentline[gdpValue]);
+    //continentObject[continent].push(asia);
+  }else if(continent == 1){
+    nAmerica[1] += parseFloat(currentline[populationValue]);
+    nAmerica[2] += parseFloat(currentline[gdpValue]);
+    //continentObject[continent].push(nAmerica);
+  }else if(continent == 2){
+    sAmerica[1] += parseFloat(currentline[populationValue]);
+    sAmerica[2] += parseFloat(currentline[gdpValue]);
+    //continentObject[continent].push(sAmerica);
+  }else if(continent == 3){
+    Australia[1] += parseFloat(currentline[populationValue]);
+    Australia[2] += parseFloat(currentline[gdpValue]);
+    //continentObject[continent].push(Australia);
+  }else if(continent == 4){
+    Europe[1] += parseFloat(currentline[populationValue]);
+    Europe[2] += parseFloat(currentline[gdpValue]);
+    //continentObject[continent].push(Europe);
+  }else if(continent == 5){
+    Africa[1] += parseFloat(currentline[populationValue]);
+    Africa[2] += parseFloat(currentline[gdpValue]);
+    //continentObject[continent].push(Africa);
+  }
+ // console.log(asia);
 }
 
 
 
 
+function getFinalContinent(){
 
+  //temp.continent = 'Asia';
+  temp.arr = asia;
+  continentObject.push(temp);
+  //temp.continent = 'Asia';
+  temp = {};
+  temp.arr = nAmerica;
+  continentObject.push(temp);
+  //temp.continent = 'Asia';
+  temp = {};
+  temp.arr = sAmerica;
+  continentObject.push(temp);
+  //temp.continent = 'Asia';
+  temp = {};
+  temp.arr = Australia;
+  continentObject.push(temp);
+  temp = {};
+  temp.arr = Europe;
+  continentObject.push(temp);
+  //temp.continent = 'Asia';
+  temp = {};
+  temp.arr = Africa;
+  continentObject.push(temp);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+  console.log(continentObject);
+}
 
 
 
